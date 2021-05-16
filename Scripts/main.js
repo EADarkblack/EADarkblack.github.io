d1109f88538860db463835f5d3dbdb50ffd35f2b
WilmarMiguez <wilmarfm156@hotmail.com>
2021-05-15 19:12:38 -0500

    Se agrego la sección de creación y se agregó funcionalidad al menú hamburguesa.

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,1 +1,38 @@
-$(".burguer-menu").svgInject();
+// Global Variables
+
+const BURGUER_ICON = document.querySelector(".burguer-icon");
+console.log(BURGUER_ICON);
+
+// Mobile Version Functionalities
+
+/**
+ * display the menu when clicked in the menu icon
+ */
+
+const menuDisplay = function() {
+    BURGUER_ICON.addEventListener("click", display);
+    function display (){
+        let menu = document.getElementById("menu");
+        menu.classList.toggle("display-menu");
+        BURGUER_ICON.classList.toggle("burguer-open-icon");
+    }
+}
+
+/**
+ * block the scroll when the menu is active
+ */
+
+const noScrolling = function() {
+    BURGUER_ICON.addEventListener("click", disableScroll);  
+    function disableScroll(){
+        let bodyNoScroll = document.getElementById("body");
+        if (bodyNoScroll.style.overflow === "hidden") {
+            bodyNoScroll.style.overflow = "initial";
+        } else {
+            bodyNoScroll.style.overflow = "hidden";
+        }
+    }
+}
+
+noScrolling();
+menuDisplay();
